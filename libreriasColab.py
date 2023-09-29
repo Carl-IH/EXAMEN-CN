25931a6572af8b234c1c8e186a05443aaba99b9c
Carlos Ibarra <125385455+Carl-IH@users.noreply.github.com>
2023-09-29 17:51:14 -0600

    Add files via upload

---
+++ libreriasColab.py
@@ -0,0 +1,86 @@
+def primo_test(valor):
+    if valor <= 1:
+        return False #ELIMINA TODOS LOS VALORES QUE SEAN NEGATIVOS Y/O MENORES A 1
+    elif valor <= 3:
+        return True
+    elif valor % 2 == 0 or valor % 3 == 0:
+        return False #SI EL VALOR SE PUEDE DIVIDIR SOBRE 2 O 3 SE ELIMINA
+    i = 5
+    while i * i <= valor:
+        print(i)
+        if valor % i == 0 or valor % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+def nextPrime(valor):
+    primo_test(valor)
+    valor +=1
+    while True:
+        if primo_test(valor):
+            return valor
+        valor += 1
+
+
+def median(valores):
+    valoresOrden = sorted(valores, reverse=True)
+    print(valoresOrden)
+    print('La mediana de tus valores es ', valoresOrden[1])
+    
+
+def randomPasword():
+    import random
+    size = random.randint(7,10)
+    password = []
+    for _ in range(size):
+        dato = chr(random.randint(33,126))
+        password.append(dato)
+
+    contrasena = ''.join(password)
+    print('tu contraseña de ', size, ' digitos es: ',contrasena)
+
+
+def hipo(a,b):
+    from math import sqrt as raiz
+    raizContenido = (a*a) + (b*b)
+    c = raiz(raizContenido)
+    print('la hipotenusa del triángulo es ',c,'u{}'.format(chr(178)))
+
+
+
+print('')
+
+#randomPasword()
+
+
+
+
+#a = int(input('escribe el valor numerico del primer lado del triángulo: '))
+#b = int(input('escribe el valor numerico del segundo lado del triángulo: '))
+
+#resultado = hipo(a,b)
+
+
+
+#valores=[]
+#for i in range(3):
+ #   valores.append(int(input("Escribe un valor numerico: ")))
+
+#print(valores)
+#mediana = median(valores)
+
+# Ejemplo de uso:
+#valor = int(input('¿Que valor desea consultar? '))
+
+
+
+#resultado = primo_test(valor)
+#print(resultado)
+#if resultado:
+ #   print(valor," es un numero primo y su siguiente numeroprimero es ",numero)
+#else:
+ #   print(valor," no es un numero primo")
+
+#resultado = nextPrime(valor)
+#print(" El siguiente numero primo a tu consulta es",resultado)
